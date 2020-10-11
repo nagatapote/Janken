61ace9a529560f0a8930d32695c131af87837e7a
nagatapote <hiroaki29p@icloud.com>
2020-10-12 07:12:10 +0900

    janken

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,10 @@
   <div id="about">
     <h3>作成日：2020/10/11</h3>
     <h3>目的：vue.js理解のため。</h3>
-    <h3>技術ブログ(pote's blog)：<a href='https://nagatapote.work/'>https://nagatapote.work/</a></h3>
-    <h3>参考サイト①：<a href='https://www.actzero.jp/developer/report-19774.html'>https://www.actzero.jp/developer/report-19774.html</a></h3>
-    <h3>参考サイト②：<a href='https://qiita.com/Mitsuzara/items/fccdb17ba14c0c9be20c'>https://qiita.com/Mitsuzara/items/fccdb17ba14c0c9be20c</a></h3>
+    <h3>技術ブログ：<a href='https://nagatapote.work/'>pote's blog</a></h3>
+    <h3>参考サイト①</h3>
+    <a href='https://www.actzero.jp/developer/report-19774.html'>https://www.actzero.jp/developer/report-19774.html</a>
+    <h3>参考サイト②</h3>
+    <a href='https://qiita.com/Mitsuzara/items/fccdb17ba14c0c9be20c'>https://qiita.com/Mitsuzara/items/fccdb17ba14c0c9be20c</a>
   </div>
 </template>
